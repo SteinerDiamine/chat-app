32b4c573410cf15a1826a4cf884a88da77ad389a
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-12-22 09:59:59 +0530

    drak and light theme added

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,18 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import { UserButton } from '@clerk/nextjs'
+
 import React from 'react'
 
 const page = () => {
   return (
+    <div>
     <UserButton 
-    afterSignOutUrl='/'/>
+    afterSignOutUrl='/'
+    />
+
+    <ModeToggle />
+
+    </div>
   )
 }
 
