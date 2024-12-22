77cdb86d14b532d7036a349136ce736c12e2170d
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-12-22 09:45:22 +0530

    sign-in added

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <UserButton 
+    afterSignOutUrl='/'/>
+  )
+}
+
+export default page
